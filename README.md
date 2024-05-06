5ec872bf5b7aa598054daf3b67335f48fb2f4922
M6a5x98 <130889212+M6a5x98@users.noreply.github.com>
2024-05-06 19:31:27 +0500

    Create placeTile function in main.js

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # carcasonne.js
 Carcasonne engine in your browser
+## Use 
+Import this script in your HTML page. Create an instance of the Game class, then provide a canvas element to the Game constructor.
--- src/main.js
+++ src/main.js
@@ -1,1 +1,34 @@
-
+class Game {
+  /**
+   *
+   * @param {HTMLCanvasElement} canvas The canvas
+   */
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.canvas.width = 700;
+    this.canvas.height = 500;
+    this.ctx = canvas.getContext("2d");
+  }
+  /**
+   *
+   * @param {{tileNumber:number; X:number; Y:number; direction:number}} tile An object containing the number of the tile, the coords of the tile and its direction
+   */
+  placeTile(tile) {
+    const mappedTile = new Image(102, 102);
+    mappedTile.src = "../img/" + tile["tileNumber"] + ".png";
+    mappedTile.onload = () => {
+      this.ctx.save();
+      this.ctx.translate(
+        tile["X"] + mappedTile.width / 2,
+        tile["Y"] + mappedTile.height / 2
+      );
+      this.ctx.rotate(tile["direction"] * (Math.PI / 180));
+      this.ctx.drawImage(
+        mappedTile,
+        -mappedTile.width / 2,
+        -mappedTile.height / 2
+      );
+      this.ctx.restore();
+    };
+  }
+}
